c7fe31f3c6f2821b6a38acd5beaa089dfc4faa0d
RonaldC67 <ra.castellanos1@gmail.com>
2016-08-08 20:50:24 -0600

    Practica 3

---
+++ app/src/main/java/com/example/ronald/cambio/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.ronald.cambio;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends Activity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button boton = (Button) findViewById(R.id.button);
+
+        boton.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+        Intent a = new Intent(this,Main2Activity.class);
+        //Inicia la actividad
+        startActivity(a);
+    }
+}
